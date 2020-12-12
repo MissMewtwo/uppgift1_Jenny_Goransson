1554a645443d8e4b654dc2475db9b6e0f3490200
JennyGöransson <goransson.jenny@gmail.com>
2020-12-12 21:02:41 +0100

    uppgift 4 uppdaterad och klar

--- uppgift4/persons/Main.java
+++ uppgift4/persons/Main.java
@@ -2,36 +2,35 @@ package persons;
 
 import javafx.application.Application;
 
-import java.io.File;
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
-import javax.xml.parsers.*;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import javafx.collections.FXCollections;
+
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellEditEvent;
-import javafx.scene.control.TablePosition;
+
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 
@@ -39,123 +38,123 @@ public class Main extends Application {
 	
 	
 	
-	TableView<Persons> table = new TableView<Persons>();
+	private TableView<Persons> table = new TableView<Persons>();
+	private final ObservableList<Persons> data =
+			FXCollections.observableArrayList();
 	
+	TextField firstNameField;
+	TextField lastNameField;
+	TextField genderField;
 	
-	TextField firstName;
-	TextField lastName;
-	TextField age;
+	public static final String XML_PATH = "persons";
 	
-	@Override
-	public void start(Stage primaryStage) {
-		
-		
-		primaryStage.setTitle("Personhantering");
-		
-		table.setEditable(true);
-		
-		
-		TableColumn<Persons, String> column1 = new TableColumn<>("First Name");
-	    column1.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-	    column1.setEditable(true);
-	    column1.setOnEditCommit(new EventHandler<CellEditEvent<Persons, String>>() {
-	        @Override
-	        public void handle(CellEditEvent<Persons, String> t) {
-	        	System.out.println("Test");
-	            ((Persons) t.getTableView().getItems().get(t.getTablePosition().getRow())).setFirstName(t.getNewValue());
-	        }
-	    });
-	   
-	   // column1.setOnEditCommit((CellEditEvent<Persons, String>event)-> {
-	    	//TablePosition<Persons, String> pos = event.getTablePosition();
-	    		
-//	    	String newFirstName = event.getNewValue();
-//	    	
-//	    	int row = pos.getRow();
-//	    	Persons person = event.getTableView().getItems().get(row);
-//	    	
-//	    	person.setFirstName(newFirstName);
-	    //});
-	    
-	    
-
-
-	    TableColumn<Persons, String> column2 = new TableColumn<>("Last Name");
-	    column2.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-	    
-	    TableColumn<Persons, String> column3 = new TableColumn<>("Age");
-	    column3.setCellValueFactory(new PropertyValueFactory<>("age"));
-	    
-	    table.getColumns().addAll(column1, column2, column3);
-	    
-	    
-	    table.setStyle("-fx-min-width: 250px;" + "-fx-min-height: 250px;" + "-fx-max-width: 250px;" + 
-				"-fx-max-height: 250px;");
-	    
-		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(10, 10, 10, 10));
-		grid.setVgap(3);
-		grid.setHgap(1);
-		
-		GridPane.setConstraints(table, 0, 10);
-		grid.getChildren().add(table);
-		
-		Label fName = new Label("First Name:");
-		GridPane.setConstraints(fName, 0, 0);
-		grid.getChildren().add(fName);
-		
-		firstName = new TextField();
-		firstName.setPrefColumnCount(10);
-		firstName.getText();
-		GridPane.setConstraints(firstName, 1, 0);
-		grid.getChildren().add(firstName);
-		
-		Label lName = new Label("Last Name:");
-		GridPane.setConstraints(lName, 0, 1);
-		grid.getChildren().add(lName);
-		
-		lastName = new TextField();
-		GridPane.setConstraints(lastName, 1, 1);
-		grid.getChildren().add(lastName);
-		
-		Label labelAge = new Label("Age:");
-		GridPane.setConstraints(labelAge, 0, 2);
-		grid.getChildren().add(labelAge);
-		
-		age = new TextField();
-		GridPane.setConstraints(age, 1, 2);
-		grid.getChildren().add(age);
-		
-		
-		Button add = new Button("Add");
-		GridPane.setConstraints(add, 0, 3);
-		grid.getChildren().add(add);
-		
-		Button update = new Button("Update");
-		GridPane.setConstraints(update, 1,3);
-		grid.getChildren().add(update);
-		
-		Button delete = new Button("Delete");
-		GridPane.setConstraints(delete, 2, 3);
-		grid.getChildren().add(delete);
-		
-		Scene scene = new Scene(grid, 500, 500);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		add.setOnAction(e -> addCode(e));
-		//delete.setOnAction(e -> deleteCode(e));
-		//update.setOnAction(e -> updateCode(e));
-		
-		readXml();
-		
-	}
-	
-	
+	final HBox hb = new HBox();
 	
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
+	
+	
+	@Override
+	public void start(Stage stage) {
+		
+		
+		Scene scene = new Scene(new Group());
+		stage.setTitle("List of interesting People");
+		stage.setWidth(450);
+		stage.setHeight(550);
+		
+		final Label label = new Label("List of interesting People");
+		label.setFont(new Font("Arial", 20));
+		
+		
+		table.setEditable(true);
+		
+		
+		TableColumn firstName = new TableColumn("First Name");
+		firstName.setMinWidth(100);
+		firstName.setCellValueFactory(
+				new PropertyValueFactory<Persons, String>("firstName"));
+		firstName.setCellFactory(TextFieldTableCell.forTableColumn());
+		firstName.setOnEditCommit(
+				new EventHandler<CellEditEvent<Persons, String>>() {
+					@Override
+					public void handle(CellEditEvent<Persons, String> t) {
+						((Persons) t.getTableView().getItems().get(t.getTablePosition().getRow())
+								).setFirstName(t.getNewValue());
+					}
+				});
+		
+	    TableColumn lastName = new TableColumn("Last Name");
+	    lastName.setMinWidth(100);
+	    lastName.setCellValueFactory(
+	    		new PropertyValueFactory<Persons, String>("lastName"));
+	   lastName.setCellFactory(TextFieldTableCell.forTableColumn());
+	   lastName.setOnEditCommit(
+			   new EventHandler<CellEditEvent<Persons, String>>() {
+				   @Override
+				   public void handle(CellEditEvent<Persons, String> t) {
+					   ((Persons) t.getTableView().getItems().get(t.getTablePosition().getRow())
+							   ).setLastName(t.getNewValue());
+				   }
+			   });
+	   
+	   TableColumn gender = new TableColumn("Gender");
+	   gender.setMinWidth(100);
+	   gender.setCellValueFactory(
+			   new PropertyValueFactory<Persons, String>("Gender"));
+	   gender.setCellFactory(TextFieldTableCell.forTableColumn());
+	   gender.setOnEditCommit(
+			   new EventHandler<CellEditEvent<Persons, String>>() {
+				   @Override
+				   public void handle(CellEditEvent<Persons, String> t) {
+					   ((Persons) t.getTableView().getItems().get(t.getTablePosition().getRow())
+							   ).setGender(t.getNewValue());
+				   }
+			   });
+	 
+	    table.setItems(data);
+	   	table.getColumns().addAll(firstName, lastName, gender);
+	   
+	    firstNameField = new TextField();
+	    firstNameField.setPromptText("First Name");
+	    firstNameField.setMaxWidth(firstName.getPrefWidth());
+	    lastNameField = new TextField();
+	    lastNameField.setPromptText("Last Name");
+	    lastNameField.setMaxWidth(lastName.getPrefWidth());
+	    genderField = new TextField();
+	    genderField.setPromptText("Gender");
+	    genderField.setMaxWidth(gender.getPrefWidth());
+	    
+	    final Button addButton = new Button("Add");
+	    addButton.setOnAction(e -> addCode(e));
+	    
+	    final Button deleteButton = new Button("Delete");
+		deleteButton.setOnAction(e -> deleteCode(e));
+		
+		
+		readXml();
+		
+		hb.getChildren().addAll(firstNameField, lastNameField, genderField, addButton, deleteButton);
+		hb.setSpacing(3);
+		
+		final VBox vbox = new VBox();
+		vbox.setSpacing(5);
+		vbox.setPadding(new Insets(10, 0, 0, 10));
+		vbox.getChildren().addAll(label, table, hb);
+		
+		((Group) scene.getRoot()).getChildren().addAll(vbox);
+		
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 
 	
@@ -163,103 +162,63 @@ public class Main extends Application {
 	
 	public void addCode(ActionEvent e) {
 		
-		Persons info = new Persons();
-		info.setFirstName(firstName.getText());
-		info.setLastName(lastName.getText());
-		info.setAge(Integer.valueOf(age.getText()));
 		
-		firstName.clear();
-		lastName.clear();
-		age.clear();
-		table.getItems().addAll(info);
+		data.add(new Persons(
+				firstNameField.getText(),
+				lastNameField.getText(),
+				genderField.getText()));
+		
+		
+		firstNameField.clear();
+		lastNameField.clear();
+		genderField.clear();
+		
 		
 		toXml();
 		
 	}
 	
 	public void deleteCode(ActionEvent e) {
-		
+		Persons selectedItem = table.getSelectionModel().getSelectedItem();
+		table.getItems().remove(selectedItem);
 		
 	}
 	
 	
 	private void toXml() {
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder docBuilder;
+		
+		XMLEncoder encoder = null;
+		
 		try {
-			docBuilder = dbFactory.newDocumentBuilder();
-			Document doc = docBuilder.newDocument();
-			Element rootElement = doc.createElement("Persons");
-			doc.appendChild(rootElement);
-			ObservableList<Persons> personArray = table.getItems();
+			encoder = new XMLEncoder (new BufferedOutputStream(new
+					FileOutputStream(XML_PATH)));
+			System.out.println("File stream opened and XMLEncoder created");
 			
-			
-			for (int i=0; i<personArray.size(); i++) {
-				Persons personData = personArray.get(i);
-				
-				Element person = doc.createElement("Person");
-				
-				person.setAttribute("firstName", personData.getFirstName());
-				person.setAttribute("lastName", personData.getLastName());
-				person.setAttribute("age", personData.getAge().toString());
-				
-				rootElement.appendChild(person);
-				
-				
-				
-			}
-			
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File("/Users/jennygoransson/personer.xml"));
-			transformer.transform(source,  result);
-			
-			StreamResult consoleResult = new StreamResult(System.out);
-			transformer.transform(source, consoleResult);
-			
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (FileNotFoundException filenNotFound) {
+			System.out.println("ERROR: While Creating or Opening the file persons.xml");
 		}
+		encoder.writeObject(data.toArray());
+		encoder.close();
 	}
+
 	
 	public void readXml() {
+		
+		XMLDecoder decoder = null;
+		
 		try {
-			File file = new File("/Users/jennygoransson/personer.xml");
+			decoder = new XMLDecoder(new BufferedInputStream(new
+					FileInputStream(XML_PATH)));
+			System.out.println("File stream opened and XMLDecoder created");
 			
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(file);
-			doc.getDocumentElement().normalize();
-			
-			NodeList nodeList = doc.getElementsByTagName("Person");
-			
-			for(int i = 0; i<nodeList.getLength(); i++) {
-				Node node = nodeList.item(i);
-				
-				NamedNodeMap nodeAttributes = node.getAttributes();
-				String firstName = nodeAttributes.getNamedItem("firstName").getNodeValue();
-				String lastName = nodeAttributes.getNamedItem("lastName").getNodeValue();
-				String age = nodeAttributes.getNamedItem("age").getNodeValue();
-				Persons info = new Persons();
-				info.setFirstName(firstName);
-				info.setLastName(lastName);
-				info.setAge(Integer.valueOf(age));
-				
-				table.getItems().addAll(info);
-				
-			}
-			
-		} catch (Exception e) {
-			
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: File persons.xml not found");
 		}
+		
+		Object[] objects = (Object[]) decoder.readObject();
+		for (Object object : objects)
+			data.add((Persons)object);
+		decoder.close();
 	}
-	
-	
-	
-	
-	
 	
 }
--- uppgift4/persons/Persons.java
+++ uppgift4/persons/Persons.java
@@ -1,30 +1,50 @@
 package persons;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Persons {
 	
-	private String firstName;
-	private String lastName;
-	private int age;
+	private final SimpleStringProperty firstName = new SimpleStringProperty();
+	private final SimpleStringProperty lastName = new SimpleStringProperty();
+	private final SimpleStringProperty gender = new SimpleStringProperty();
 	
+	public Persons() {}
+		
+	
+	
+	
+	public Persons (String fName, String lName, String gen) {
+		setFirstName(fName);
+		setLastName(lName);
+		setGender(gen);
+		
+	}
 	
 	public String getFirstName() {
-		return firstName;
+		return firstName.get();
 	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	
+	public void setFirstName(String fName) {
+		firstName.set(fName);
 	}
+	
 	public String getLastName() {
-		return lastName;
+		return lastName.get();
 	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	
+	public void setLastName(String lName) {
+		lastName.set(lName);
 	}
-	public Integer getAge() {
-		return age;
+	
+	public String getGender() {
+		return gender.get();
 	}
-	public void setAge(int age) {
-		this.age = age;
+	
+	public void setGender(String gen) {
+		gender.set(gen);
 	}
+	
+
 	
 	
 
